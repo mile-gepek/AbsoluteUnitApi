58e921f8d6f76e6b1abec4479a207327a627494b
mile gepek <theontley@gmail.com>
2025-10-09 16:56:42 +0200

    Refactored main to bot.py

--- absolute_unit/__init__.py
+++ absolute_unit/__init__.py
@@ -1,9 +1,24 @@
+import logging
+
 from pint import UnitRegistry
 
 
-__all__ = [
-    "ureg",
-]
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="absolute_unit.log", encoding="utf-8", mode="w")
+handler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
+logger.addHandler(handler)
+
+disnake_logger = logging.getLogger("disnake")
+disnake_logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="disnake.log", encoding="utf-8", mode="w")
+handler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
+disnake_logger.addHandler(handler)
+_ = logging.getLogger(__name__)
 
 
 ureg = UnitRegistry()
